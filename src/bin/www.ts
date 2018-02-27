88f16e26a104a250fd32aa99e5d654e0e0bdaa03
Nayeema Sadeque <nstonnz@Nayeemas-MBP.local>
2018-02-27 16:51:24 +0100

    commit script to set env variables

---
+++ src/bin/www.ts
@@ -0,0 +1,15 @@
+
+import * as dotenv from "dotenv";
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: ".env"});
+}
+// get the current environment
+const env: any = process.env.NODE_ENV;
+// convert to uppercase
+const envString: string = env.toUpperCase();
+
+process.env.MONGODB_URI = process.env["MONGODB_URI_" + envString];
+process.env.PORT = process.env["PORT_" + envString];
+process.env.JWT_SECRET = process.env["JWT_SECRET_" + envString];
+
+import "../index";
