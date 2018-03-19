a0470fc967c58a79c8f91460aa697e4c2f409722
Nayeema Sadeque <nstonnz@Nayeemas-MBP.local>
2018-03-19 19:10:27 +0100

    refactored method names

--- src/routers/availabilityRouter.ts
+++ src/routers/availabilityRouter.ts
@@ -3,6 +3,7 @@ import Availability from "../models/availability";
 import { authenticate } from "../middlewares/authenticate";
 import { validateObjectID } from "../middlewares/validateObjectID";
 import * as _ from "lodash";
+import { successhandler } from "../middlewares/successhandler";
 
 export class AvailabilityRouter {
     public router: Router;
@@ -11,100 +12,86 @@ export class AvailabilityRouter {
         this.router = Router();
         this.routes();
     }
+
+    // get all availabilities
+    // get all availabilities for each employee: employee id
+    // add availability for an employee: employee id
+    // delete availability: availability id
+    // update availability: availability id
     public async getAvailabilities(req: Request, res: Response, next: NextFunction) {
         try {
-            const data = await Availability.find({
+            const result = await Availability.find({
                 _creator: req.body.user._id
             });
-            const status = res.statusCode;
-            res.json({
-                status,
-                data
-            });
+            successhandler(result, req, res, next);
         } catch (err) {
             next(err);
         }
     }
-    public async getAvailability(req: Request, res: Response, next: NextFunction) {
+    public async getAvailabilityByEmployeeId(req: Request, res: Response, next: NextFunction) {
         try {
-            const data = await Availability.find({
+            const result = await Availability.find({
                 _id: req.params.id,
                 _creator: req.body.user._id
             });
-            if (!data) {
-                const err = new Error ("data not found");
+            if (!result) {
+                const err = new Error ("Data not found");
                 res.statusCode = 404;
                 next(err);
             }
-            const status = res.statusCode;
-            res.json({
-                status,
-                data
-            });
+            successhandler(result, req, res, next);
         } catch (err) {
             next(err);
         }
-  }
-    public async createAvailability(req: Request, res: Response, next: NextFunction) {
+    }
+    public async addAvailabilityByEmployeeId(req: Request, res: Response, next: NextFunction) {
         try {
             const _creator = req.body.user._id;
             const body = _.pick(req.body, ["day", "time"]);
             const availability = new Availability({ ...body, _creator });
-            const data = await availability.save();
-            const status = res.statusCode;
-            res.json({
-                status,
-                data
-            });
+            const result = await availability.save();
+            successhandler(result, req, res, next);
         } catch (err) {
             next(err);
         }
-  }
-    public async deleteAvailability(req: Request, res: Response, next: NextFunction) {
+    }
+    public async deleteAvailabilityByEmployeeId(req: Request, res: Response, next: NextFunction) {
         try {
-            const data = await Availability.findOneAndRemove({
+            const result = await Availability.findOneAndRemove({
                 _id: req.params.id,
                 _creator: req.body.user._id
             });
-            if (!data) {
-                const err = new Error ("data not found");
+            if (!result) {
+                const err = new Error ("Data not found");
                 res.statusCode = 404;
                 next(err);
             }
-            const status = res.statusCode;
-            res.json({
-                status,
-                data
-            });
+            successhandler(result, req, res, next);
         } catch (err) {
             next(err);
         }
-  }
-    public async updateAvailability(req: Request, res: Response, next: NextFunction) {
+    }
+    public async updateAvailabilityByEmployeeId(req: Request, res: Response, next: NextFunction) {
         try {
-            const data = await Availability.findOneAndUpdate({ _id: req.params.id, _creator: req.body.user._id },
+            const result = await Availability.findOneAndUpdate({ _id: req.params.id, _creator: req.body.user._id },
                 { $set: req.body },
                 { runValidators: true, new: true });
-            if (!data) {
-                const err = new Error ("data not found");
+            if (!result) {
+                const err = new Error ("Data not found");
                 res.statusCode = 404;
                 next(err);
             }
-            const status = res.statusCode;
-            res.json({
-                status,
-                data
-            });
+            successhandler(result, req, res, next);
         } catch (err) {
             next(err);
         }
-  }
+    }
     public routes() {
         this.router.get("/", authenticate, this.getAvailabilities);
-        this.router.get("/:id", [authenticate, validateObjectID], this.getAvailability);
-        this.router.post("/", authenticate, this.createAvailability);
-        this.router.delete("/:id", [authenticate, validateObjectID], this.deleteAvailability);
-        this.router.put("/:id", [authenticate, validateObjectID], this.updateAvailability);
+        this.router.post("/", authenticate, this.addAvailabilityByEmployeeId);
+        this.router.get("/:id", [authenticate, validateObjectID], this.getAvailabilityByEmployeeId);
+        this.router.delete("/:id", [authenticate, validateObjectID], this.deleteAvailabilityByEmployeeId);
+        this.router.put("/:id", [authenticate, validateObjectID], this.updateAvailabilityByEmployeeId);
     }
 }
 // export
